2302468958e34d0f18778f8d2aff65fbe19e7c9d
yinliu <yinliu.tiger@foxmail.com>
2024-10-21 14:45:26 +0800

    [Update] (1)To make the native_droid part can be successful compiled (nativedroid/setup.py) (2)make the benchmark commands work well(benchmark_cli.sh) (3)using virtualenv, so need to ignore some files and folders (change in the .gitignore)

--- tools/scripts/benchmark_cli.sh
+++ tools/scripts/benchmark_cli.sh
@@ -7,24 +7,24 @@ BASEDIR=$(dirname "$0")
 python $BASEDIR/start_nativedroid_server.py /tmp/binaries/ localhost 50051 localhost 55001 ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/NativeSourcesAndSinks.txt ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt &
 nativedroid_pid=$!
 
-java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.1.4-SNAPSHOT-assembly.jar jnsaf /tmp/apks/ 55001 localhost 50051 &
+java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.2.1-SNAPSHOT-assembly.jar jnsaf /tmp/apks/ 55001 localhost 50051 &
 jnsaf_pid=$!
 
 sleep 5
 
 droid_bench_submitter()
 {
-    java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.1.4-SNAPSHOT-assembly.jar benchmark -a COMPONENT_BASED $BASEDIR/../../benchmarks/DroidBench localhost 55001 $BASEDIR/../../benchmarks/expected_droid_bench.txt
+    java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.2.1-SNAPSHOT-assembly.jar benchmark -a COMPONENT_BASED $BASEDIR/../../benchmarks/DroidBench localhost 55001 $BASEDIR/../../benchmarks/expected_droid_bench.txt
 }
 
 icc_bench_submitter()
 {
-    java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.1.4-SNAPSHOT-assembly.jar benchmark -a COMPONENT_BASED $BASEDIR/../../benchmarks/ICCBench localhost 55001 $BASEDIR/../../benchmarks/expected_icc_bench.txt
+    java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.2.1-SNAPSHOT-assembly.jar benchmark -a COMPONENT_BASED $BASEDIR/../../benchmarks/ICCBench localhost 55001 $BASEDIR/../../benchmarks/expected_icc_bench.txt
 }
 
 native_flow_bench_submitter()
 {
-    java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.1.4-SNAPSHOT-assembly.jar benchmark -a BOTTOM_UP $BASEDIR/../../benchmarks/NativeFlowBench localhost 55001 $BASEDIR/../../benchmarks/expected_nativeflow_bench.txt
+    java -jar $BASEDIR/../../target/scala-2.12/argus-saf-3.2.1-SNAPSHOT-assembly.jar benchmark -a BOTTOM_UP $BASEDIR/../../benchmarks/NativeFlowBench localhost 55001 $BASEDIR/../../benchmarks/expected_nativeflow_bench.txt
 }
 
 MODE='ALL'
